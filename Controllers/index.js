0ae35ef38a2c53de3e71870934b28cba936def8f
georginaw <watson.georgina@gmail.com>
2019-12-11 11:07:20 +0000

    added the store token with expiry function and called it from within the generate token function. Handled not happy paths (db connection issues) and tidied up JSON response code

--- Controllers/index.js
+++ Controllers/index.js
@@ -2,15 +2,23 @@ const authService = require('../Services/AuthService');
 const TokenService = require('../Services/TokenService');
 
 async function verifyPinGenerateToken(req, res) {
+    let responseData = {'success': false, 'data': '', 'msg': 'pass code error'};
     let pin = req.body.pin;
-    // validate
-    let verification = await authService.verifyPin(pin);
-    if (verification) {
-        let token = TokenService.generateToken();
-        res.json({'success': true, 'data': token, 'msg': 'pass code verified'});
-    } else {
-        res.json({'success': false, 'data': '', 'msg': 'pass code error'});
+    // validate (if pin 4 digits fo below otherwise if doeesn't exist or is not 4 numbers sad path)
+    // sad path responseData.msg = 'incorrect pin';
+    // happy path:
+    try {
+        responseData.success = await authService.verifyPin(pin);
+        if (responseData.success) {
+            responseData.data = await TokenService.generateToken();
+            responseData.msg = 'pass code verified';
+        }
+    } catch(e) {
+        responseData.msg = 'unexpected error';
     }
+
+    res.json(responseData);
+
 }
 
 module.exports.verifyPinGenerateToken = verifyPinGenerateToken;
--- Services/TokenService/index.js
+++ Services/TokenService/index.js
@@ -1,7 +1,23 @@
 const base64 = require('base-64');
+const dbConnector = require('../DbService');
+const fiveMinutes = 300;
 
-function generateToken() {
-    return base64.encode(new Date());
+async function generateToken() {
+    let token = base64.encode(new Date());
+    try {
+        let stored = await storeToken(token);
+        return token;
+    } catch(e) {
+        return 'unexpected error';
+    }
+}
+
+async function storeToken(token) {
+    let db = await dbConnector();
+    let col = db.collection('tokens');
+    let expiry = new Date().getTime() + fiveMinutes;
+    let storedToken = await col.insertOne({'token': token, 'expiry':  expiry});
+    return storedToken;
 }
 
 module.exports.generateToken = generateToken;
